fea67982251369408aa847804f1c7125f8501585
Owen Zhang <ozhang@micds.org>
2022-10-24 09:31:16 -0500

    PA07 done maybe

--- PA07/PA07.cpp
+++ PA07/PA07.cpp
@@ -10,28 +10,6 @@ struct person
     string mail;
     int pos;
 };
-void split(const string &str, const string &delim, vector<string> &parts)
-{
-    size_t start, end = 0;
-    while (end < str.size())
-    {
-        start = end;
-        while (start < str.size() && (delim.find(str[start]) != string::npos))
-        {
-            start++; // skip initial whitespace
-        }
-        end = start;
-        while (end < str.size() && (delim.find(str[end]) == string::npos))
-        {
-            end++; // skip to end of word
-        }
-        if (end - start != 0)
-        { // just ignore zero-length strings.
-            parts.push_back(string(str, start, end - start));
-        }
-    }
-}
-
 void writeData(string output_file, vector<person> w)
 {
     ofstream output(output_file);
@@ -80,5 +58,3 @@ int main(int argc, char *argv[])
     }
     writeData(myFile, people);
 }
-
-
